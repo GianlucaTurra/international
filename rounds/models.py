276794168754e2b96f67fd529d33a5720168eba1
GianlucaTurra <gianlucaturra1@gmail.com>
2025-05-26 17:30:58 +0200

    Rounds app

---
+++ rounds/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from tournaments.models import Tournament
+
+
+class Round(models.Model):
+    """Model definition for Round."""
+
+    number = models.IntegerField()
+    tournament = models.ForeignKey(
+        Tournament, on_delete=models.CASCADE, related_name="rounds"
+    )
+
+    class Meta:
+        """Meta definition for Round."""
+
+        db_table = "rounds"
+        verbose_name = "Round"
+        verbose_name_plural = "Rounds"
+
+    def __str__(self):
+        """Unicode representation of Round."""
+        return f"Round n.{self.number}"
