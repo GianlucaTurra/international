00b9dafa51fbaeb5fa77a7d226cad0fdeb56c397
GianlucaTurra <gianlucaturra1@gmail.com>
2025-06-15 23:24:01 +0200

    Rounds api for saving results (incomplete of response)

--- rounds/api.py
+++ rounds/api.py
@@ -1,24 +1,53 @@
+from typing import List
+
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404
 from ninja import Router
 
-from rounds.schemas import RoundIn, RoundOut
-from pairings.models import Pairing
-
+from pairings.models import Pairing, PlayerEntry
 from rounds.models import Round
+from rounds.schemas import RoundIn, RoundOut
+from standings.models import Standing
 
 router = Router()
+
+
+@router.put("/save", response={200: RoundOut})
+def save_round(request: HttpRequest, payload: RoundIn):
+    current_round = get_object_or_404(Round, pk=payload.id)
+    for req_pairing in payload.pairings:
+        pairing: Pairing = get_object_or_404(Pairing, pk=req_pairing.id)
+
+        # Update entries with given results
+        updated_entries: List[PlayerEntry] = []
+        old_entries: List[PlayerEntry] = list(pairing.entries.all())  # type: ignore
+        for old_entry, new_entry in zip(old_entries, req_pairing.entries):  # type: ignore
+            get_object_or_404(PlayerEntry, pk=new_entry.id)
+            old_entry.wins = new_entry.wins
+            updated_entries.append(old_entry)
+        updated_rows: int = PlayerEntry.objects.bulk_update(updated_entries, ["wins"])
+        assert updated_rows == len(updated_entries)  # TODO: handle this case
+
+        # Based on update entries update the standings
+        # TODO: for now let's assume all matches are 1v1
+        first_entry = updated_entries[0]
+        second_entry = updated_entries[1]
+        first_player_standing = Standing.objects.get(player=first_entry.player)
+        second_player_standing = Standing.objects.get(player=second_entry.player)
+        first_player_standing.games_won += first_entry.wins
+        second_player_standing.games_won += second_entry.wins
+        if first_entry.wins > second_entry.wins:
+            first_player_standing.matches_won += 1
+        if second_entry.wins > first_entry.wins:
+            second_player_standing.matches_won += 1
+        Standing.objects.bulk_update(
+            [first_player_standing, second_player_standing],
+            ["games_won", "matches_won"],
+        )
+    current_round.refresh_from_db()
+    return current_round
 
 
 @router.get("/{round_id}", response={200: RoundOut})
 def get_round(request: HttpRequest, round_id: int):
     return get_object_or_404(Round, pk=round_id)
-
-
-@router.post("/save")
-def save_round(request: HttpRequest, payload: RoundIn):
-    current_round: Round = get_object_or_404(Round, pk=payload.id)
-    for req_pairing in payload.pairings:
-        pairing: Pairing = get_object_or_404(Pairing, pk=req_pairing.id)
-        for old_entry, new_entry in zip(pairing.entries.all(), req_pairing.entries):
-            pass
--- rounds/functions.py
+++ rounds/functions.py
@@ -1,7 +1,10 @@
 import random
+from typing import List
 
 from pairings.models import Pairing, PlayerEntry
+from players.models import Player
 from rounds.models import Round
+from standings.models import Standing
 from tournaments.models import Tournament
 
 
@@ -9,11 +12,28 @@ def generate_first_round(tournament: Tournament) -> None:
     first_round = Round.objects.create(number=1, tournament=tournament)
     player_list = list(tournament.players.all())
     random.shuffle(player_list)
+    create_pairings(first_round, player_list)
+    create_standings(tournament, player_list)
+
+
+def create_pairings(first_round: Round, player_list: List[Player]):
     first_half, second_half = (
         player_list[: len(player_list) // 2],
         player_list[len(player_list) // 2 :],
     )
+    pairings: List[Pairing] = []
+    players_entries: List[PlayerEntry] = []
     for p1, p2 in zip(first_half, second_half):
-        pairing = Pairing.objects.create(round=first_round)
-        PlayerEntry.objects.create(pairing=pairing, player=p1)
-        PlayerEntry.objects.create(pairing=pairing, player=p2)
+        pairing = Pairing(round=first_round)
+        pairings.append(pairing)
+        players_entries.append(PlayerEntry(pairing=pairing, player=p1))
+        players_entries.append(PlayerEntry(pairing=pairing, player=p2))
+    Pairing.objects.bulk_create(pairings)
+    PlayerEntry.objects.bulk_create(players_entries)
+
+
+def create_standings(tournament: Tournament, player_list: List[Player]):
+    standings: List[Standing] = []
+    for player in player_list:
+        standings.append(Standing(tournament=tournament, player=player))
+    Standing.objects.bulk_create(standings)
