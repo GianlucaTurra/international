3aaa000172194a139ae347e2de59f6d90e62ff07
GianlucaTurra <gianlucaturra1@gmail.com>
2025-06-18 16:31:28 +0200

    Users in api docs

--- international/api.py
+++ international/api.py
@@ -3,6 +3,7 @@ from ninja import NinjaAPI
 from players.api import router as players_router
 from rounds.api import router as rounds_router
 from tournaments.api import router as tournaments_router
+from users.api import router as users_router
 from ninja_jwt.routers.blacklist import blacklist_router
 from ninja_jwt.routers.obtain import obtain_pair_router
 from ninja_jwt.routers.verify import verify_router
@@ -15,3 +16,4 @@ api.add_router("/token", obtain_pair_router, tags=["auth"])
 api.add_router("/verify", verify_router, tags=["auth"])
 api.add_router("/blacklist", blacklist_router, tags=["auth"])
 api.add_router("/rounds", rounds_router, tags=["rounds"])
+api.add_router("/users", users_router, tags=["users"])
